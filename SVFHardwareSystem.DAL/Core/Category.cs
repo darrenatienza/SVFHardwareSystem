f228b94574edd95b26618e28dd5452a118de044f
Darren Atienza <darrenatienza@gmail.com>
2020-09-15 16:56:39 -0700

    Add new parts

---
+++ SVFHardwareSystem.DAL/Core/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVFHardwareSystem.DAL.Core
+{
+    public class Category
+    {
+        public Category() { }
+
+        public int CategoryID {get; set;}
+        public string Name { get; set; }
+    }
+}
---
+++ SVFHardwareSystem.DAL/Core/ProductInventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVFHardwareSystem.DAL.Core
+{
+    public class ProductInventory
+    {
+        public ProductInventory() { }
+        public int ProductInventoryID { get; set; }
+        public DateTime CreateTimeStamp { get; set; }
+        public int ProductID { get; set; }
+
+    }
+}
